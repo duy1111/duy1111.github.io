ff69c0ab6fae87cd672d56f8fcd54368e5c8ffb8
Duy <maixuanduy0605@gmail.com>
2021-03-31 16:21:19 +0700

    sua

--- hoc/sp.js
+++ hoc/sp.js
@@ -1,10 +1,4 @@
-CheckName = (fullname) =>{
-    fullname = String(fullname);
-    for(let i =0 ;i<ten.lenght;i++){
-        if(fullname.charCodeAt(i)>=48 && fullname.charCodeAt(i)<=57) return false;
-    }
-    return true;
-}
+
 document.getElementById("form").onsubmit = (e) => {
     e.preventDefault();
     ten = document.getElementById("full-name").value;
