d4edac759370eda7fd5b1f1ed6a7313686db56af
Duy <maixuanduy0605@gmail.com>
2021-04-01 22:23:00 +0700

    dark

--- hoc/sp.js
+++ hoc/sp.js
@@ -1,7 +1,27 @@
-
+function kiemtra(na, age){
+    na = String(na); //chuyển về chuổi để tránh null 
+    if(na.length==0){
+        return false;
+    }
+    if(age.length==0){
+        return false;
+    }
+    if(age<=0||age>=100){
+        return false;
+    }
+    for(let i = 0; i<na.length;i++){
+        if(na.charCodeAt(i)>=48&&na.charCodeAt(i)<=57){
+            return false;
+        }
+    }
+    return true;
+}
 document.getElementById("form").onsubmit = (e) => {
     e.preventDefault();
     ten = document.getElementById("full-name").value;
     tuoi = document.getElementById("age").value;
-    document.getElementById("result").innerHTML += "Ten: " + ten + " ,tuoi: " + tuoi + "<br>";
+    if(kiemtra(ten,tuoi)){
+        document.getElementById("result").innerHTML += "Ten: " + ten + " ,tuoi: " + tuoi + "<br>";
+    }
+    else alert('nhap sai'); 
 }
